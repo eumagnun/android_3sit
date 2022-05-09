bd14c80261b38f124c7854c422fc2e1b72aa39bc
Daniel Amaral <eumagnun@gmail.com>
2022-05-09 19:01:21 -0300

    jogodomilhao-parte1

---
+++ jogodomilhao/app/src/main/java/br/com/danielamaral/jogo_do_milhao/GanhouFragment.kt
@@ -0,0 +1,21 @@
+package br.com.danielamaral.jogo_do_milhao
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class GanhouFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ganhou, container, false)
+    }
+
+}
